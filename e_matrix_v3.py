c9f44fc070ca7a3e6b2bf855aad84be0ac9721a4
JJTilt <jiajie.feng@tiltrenewables.com>
2025-06-04 16:49:59 +1000

    Update e_matrix_v3.py

--- e_matrix_v3.py
+++ e_matrix_v3.py
@@ -97,7 +97,7 @@ with col1:
     }
 
     # Background zones
-    ax.add_patch(patches.Rectangle((2.5, 2.5), 2.5, 2.5, color=colors['Do']))
+    ax.add_patch(patches.Rectangle((2.5, 2.5), 3, 3, color=colors['Do']))
     ax.add_patch(patches.Rectangle((0, 2.5), 2.5, 2.5, color=colors['Delegate']))
     ax.add_patch(patches.Rectangle((2.5, 0), 2.5, 2.5, color=colors['Schedule']))
     ax.add_patch(patches.Rectangle((0, 0), 2.5, 2.5, color=colors['Eliminate']))
